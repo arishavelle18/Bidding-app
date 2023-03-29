fe9a6c3b8c663fe08d4d7ec755812f9f22bcc967
arishavelle18 <arishavelle18@yahoo.com>
2023-03-30 03:27:22 +0800

    fixing admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,28 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-    permit_params :full_name, :email, :password,:password_confirmation, :is_admin
+    permit_params :id,:full_name, :email, :password,:password_confirmation,:created_at,:updated_at
+   
+    index do
+      selectable_column
+      id_column
+      column :full_name
+      column :email
+      column :password_digest
+      actions
+    end
+    show do
+      attributes_table do
+        row :id
+        row :full_name
+        row :email
+        row :password_digest
+        row :created_at
+        row :updated_at
+      end
+      
+    end
+    
 
     form do |f|
       f.semantic_errors *f.object.errors.attribute_names
@@ -14,7 +35,6 @@ ActiveAdmin.register User do
         f.input :email
         f.input :password
         f.input :password_confirmation
-        f.input :is_admin
       end
       f.actions
     end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,21 +1,37 @@
 class AdminUser < ApplicationRecord
-  after_create :create_user_record
-  private
+  # after_create :create_user_record
+  # private
 
-  def create_user_record
-    user = User.new(full_name:full_name,email: email, password:password,password_confirmation:password,is_admin:true)
+  # def create_user_record
+  #   user = User.new(full_name:full_name,email: email, password:password,password_confirmation:password,is_admin:true)
 
-    if user.valid?
-      user.save
-    else
-      # ipapakita ang validation errors ng user model
-      errors.add(:base, user.errors.full_messages.join(', '))
-      raise ActiveRecord::Rollback
-    end
-  end 
+  #   if user.valid?
+  #     user.save
+  #   else
+  #     # ipapakita ang validation errors ng user model
+  #     errors.add(:base, user.errors.full_messages.join(', '))
+  #     raise ActiveRecord::Rollback
+  #   end
+  # end 
   
   validates :full_name,presence:true,length:{maximum:50},format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
-  
+  before_save :downcase_email
+
+    def downcase_email
+        self.email = email.downcase
+    end
+
+    # check if the column is presence 
+    # check also the length
+    # only allow letters and spaces
+    # email must be unique
+    validates :full_name,presence:true,length:{maximum:50},format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
+    # i use valid_email2 gem for advance evaluation of email
+    validates :email,presence:true,'valid_email_2/email': true,uniqueness:true
+    # my password must contain uppercase,lowercase,digit and special character
+    validates :password,presence:true,length:{minimum:8},
+    format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+\z/,
+     message: "must contain at least one lowercase letter, one uppercase letter, one digit, and one special character" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
